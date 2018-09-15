32b0789a59548822e691e7cbfa705c330d1464f5
Yifei Zhang <yifei7.zhang@gmail.com>
2018-09-15 18:39:08 -0400

    Added piece moement functionality
    started working on pawn movement

---
+++ Assets/Scripts/PieceMovement.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PieceMovement : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    class Pawn
+    {
+        int positionAlpha;
+        int positionNum;
+        int targetPositionAlpha;
+        int targetPositionNum;
+        int moves = 0;
+
+        public void MovePawn()
+        {
+            bool checkAlpha = false;
+            bool checkNum = false;
+            if (moves == 0)
+            {
+                if (targetPositionNum == positionNum + 1 || targetPositionNum == positionNum + 2)){
+                    checkNum = true;
+                }
+
+            }
+        }
+    }
+
+    class Rook
+    {
+        int positionAlpha;
+        int positionNum;
+    }
+
+    class Bishop
+    {
+        int positionAlpha;
+        int positionNum;
+    }
+
+    class Knight
+    {
+        int positionAlpha;
+        int positionNum;
+    }
+
+    class Queen
+    {
+        int positionAlpha;
+        int positionNum;
+    }
+
+    class King
+    {
+        int positionAlpha;
+        int positionNum;
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
