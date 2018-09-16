9e3dce860b84e57006b441716e954405f5cd3f13
Ani Srikanth <ani.srikanth23@gmail.com>
2018-09-16 07:23:16 -0400

    Revert "DONE WATSIN SPEECH TO VOICE INTEGRATION"
    This reverts commit 252651b47dee6f607c8a5456346796685b0c5007.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,45 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using IBM.Watson.DeveloperCloud.Logging;
-using IBM.Watson.DeveloperCloud.Services.SpeechToText.v1;
-using IBM.Watson.DeveloperCloud.Utilities;
 using UnityEngine;
-using IBM.Watson.DeveloperCloud.Connection;
 
 public class PlayerController : MonoBehaviour
 {
     public int speed = 0;
-    private int minFreq;
-    private int maxFreq;
-    private AudioSource goAudioSource;
-    private AudioClip clip;
-    private AudioClip _audioClip;
-    private SpeechToText _speechToText;
-
-
-    // private boolean firstPressed = false, firstReleased = false,secondPressed = false, secondReleased = true;
-
-    // private double timeSinceStart = 0;
 
     // Use this for initialization
     void Start()
     {
 
-        Microphone.GetDeviceCaps(null, out minFreq, out maxFreq);
-        goAudioSource = GetComponent<AudioSource>();
-
-        _speechToText = new SpeechToText(new Credentials("334f8ed4-4f4a-4b78-a211-765d9174e94a", "RimNDl2eS8ZZ", "https://stream.watsonplatform.net/speech-to-text/api"));
     }
 
     // Update is called once per frame
     void Update()
     {
 
-        if (Input.GetKeyDown("space")) //OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger)
-        {
-            Debug.Log("udashaas");
-            StartCoroutine(Test());
-
+        if(OVRInput.IsControllerConnected(OVRInput.Controller.RTrackedRemote)|| OVRInput.IsControllerConnected(OVRInput.Controller.LTrackedRemote)){
+            if(OVRInput.Get(OVRInput.RawButton.RIndexTrigger)|| OVRInput.Get(OVRInput.RawButton.LIndexTrigger))
+            {
+                SingleMicrophoneCapture mic = new SingleMicrophoneCapture();
+            }
         }
 
         // get input data from keyboard or controller
@@ -53,49 +34,4 @@ public class PlayerController : MonoBehaviour
         transform.position = position;
 
     }
-
-    public AudioClip getAudio()
-    {
-        return this.clip;
-    }
-
-    IEnumerator Test()
-    {
-        AudioSource aud = GetComponent<AudioSource>();
-        Debug.Log("1");
-        aud.clip = Microphone.Start(null, false, 4, 44100);
-        Debug.Log("2");
-
-        yield return new WaitForSeconds(3);
-
-        Microphone.End(null);
-        clip = aud.clip;
-        //aud.Play();
-        Recognize();
-        Debug.Log("3");
-    }
-
-    private void Recognize()
-    {
-        //  create AudioClip with clip bytearray data
-        _audioClip = clip;
-
-        _speechToText.Keywords = new string[]{ "E1", "E2", "E3", "E4", "E5" };
-        _speechToText.KeywordsThreshold = 0.3f;
-        if (!_speechToText.Recognize(HandleRecognize, OnFail,_audioClip))
-            Debug.Log("ExampleSpeechToText.Recognize() Failed to recognize!");
-    }
-
-    private void OnFail(RESTConnector.Error error, Dictionary<string, object> customData){
-        Debug.Log("!" + error);
-        Debug.Log("!" + customData);
-        Debug.Log("NaNi");
-    }
-
-    private void HandleRecognize(SpeechRecognitionEvent result, Dictionary<string, object> customData)
-    {
-        Debug.Log("ExampleSpeechToText.HandleRecognize() Speech to Text - Get model response: " + customData["json"].ToString());
-
-    }
-
 }
--- Assets/Scripts/SingleMicrophoneCapture.cs
+++ Assets/Scripts/SingleMicrophoneCapture.cs
@@ -52,35 +52,31 @@ public class SingleMicrophoneCapture : MonoBehaviour
             //If the audio from any microphone isn't being captured  
             if (!Microphone.IsRecording(null))
             {
-                //Case the 'Record' button gets pressed
-
-                if (OVRInput.IsControllerConnected(OVRInput.Controller.RTrackedRemote) || OVRInput.IsControllerConnected(OVRInput.Controller.LTrackedRemote))
+                //Case the 'Record' button gets pressed  
+                if (GUI.Button(new Rect(Screen.width / 2 - 100, Screen.height / 2 - 25, 200, 50), "Record"))
                 {
-                    if (GUI.Button(new Rect(Screen.width / 2 - 100, Screen.height / 2 - 25, 200, 50), "Record"))
-                    {
-                        //Start recording and store the audio captured from the microphone at the AudioClip in the AudioSource  
-                        goAudioSource.clip = Microphone.Start(null, true, 4, maxFreq);
-                    }
-                }
-                else //Recording is in progress  
-                {
-                    //Case the 'Stop and Play' button gets pressed  
-                    if (GUI.Button(new Rect(Screen.width / 2 - 100, Screen.height / 2 - 25, 200, 50), "Stop and Play!"))
-                    {
-                        Microphone.End(null); //Stop the audio recording  
-                        goAudioSource.Play(); //Playback the recorded audio  
-                    }
-
-                    GUI.Label(new Rect(Screen.width / 2 - 100, Screen.height / 2 + 25, 200, 50), "Recording in progress...");
+                    //Start recording and store the audio captured from the microphone at the AudioClip in the AudioSource  
+                    goAudioSource.clip = Microphone.Start(null, true, 20, maxFreq);
                 }
             }
-            else // No microphone  
+            else //Recording is in progress  
             {
-                //Print a red "Microphone not connected!" message at the center of the screen  
-                GUI.contentColor = Color.red;
-                GUI.Label(new Rect(Screen.width / 2 - 100, Screen.height / 2 - 25, 200, 50), "Microphone not connected!");
-            }
+                //Case the 'Stop and Play' button gets pressed  
+                if (GUI.Button(new Rect(Screen.width / 2 - 100, Screen.height / 2 - 25, 200, 50), "Stop and Play!"))
+                {
+                    Microphone.End(null); //Stop the audio recording  
+                    goAudioSource.Play(); //Playback the recorded audio  
+                }
 
+                GUI.Label(new Rect(Screen.width / 2 - 100, Screen.height / 2 + 25, 200, 50), "Recording in progress...");
+            }
         }
+        else // No microphone  
+        {
+            //Print a red "Microphone not connected!" message at the center of the screen  
+            GUI.contentColor = Color.red;
+            GUI.Label(new Rect(Screen.width / 2 - 100, Screen.height / 2 - 25, 200, 50), "Microphone not connected!");
+        }
+
     }
 }
